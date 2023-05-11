b832f05d2daf0413917f0e521a08eeaafa7e64a6
Adam-board <adamboard2003@gmail.com>
2023-05-11 17:41:25 +0100

    Implemented Movement to TodoList and Goals

--- PlanningBoard/app/src/main/java/uk/ac/abertay/planningboard/LandingPage.kt
+++ PlanningBoard/app/src/main/java/uk/ac/abertay/planningboard/LandingPage.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import uk.ac.abertay.planningboard.databinding.LandingFragmentBinding
 
 class LandingPage : Fragment() {
@@ -15,6 +16,15 @@ class LandingPage : Fragment() {
     ): View? { val binding: LandingFragmentBinding = DataBindingUtil.inflate(
         inflater, R.layout.landing_fragment, container, false
     )
+
+
+        binding.gotoGoals.setOnClickListener{view :View->
+        view.findNavController().navigate(R.id.action_landingPage_to_goalsFragment)
+        }
+        binding.gotoTodo.setOnClickListener{view:View ->
+        view.findNavController().navigate(R.id.action_landingPage_to_todoFragment)
+        }
+
 
         return binding.root
     }
--- PlanningBoard/app/src/main/java/uk/ac/abertay/planningboard/MainActivity.kt
+++ PlanningBoard/app/src/main/java/uk/ac/abertay/planningboard/MainActivity.kt
@@ -11,12 +11,14 @@ import uk.ac.abertay.planningboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var appBarConfiguration: AppBarConfiguration
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
     }
+
+
 }
