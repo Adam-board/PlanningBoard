826336a1c2d6ccaacf7ef20743387330cbb3a511
Adam-board <adamboard2003@gmail.com>
2023-05-13 00:27:22 +0100

    Created DrawerLayout that includes links to other fragments

--- PlanningBoard/app/src/main/java/uk/ac/abertay/planningboard/MainActivity.kt
+++ PlanningBoard/app/src/main/java/uk/ac/abertay/planningboard/MainActivity.kt
@@ -2,6 +2,7 @@ package uk.ac.abertay.planningboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -11,14 +12,21 @@ import uk.ac.abertay.planningboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var toggle : ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        
+
+        }
     }
 
 
-}
---
+++ PlanningBoard/app/src/main/java/uk/ac/abertay/planningboard/QuotesGenerator.kt
@@ -0,0 +1,4 @@
+package uk.ac.abertay.planningboard
+
+class QuotesGenerator {
+}
